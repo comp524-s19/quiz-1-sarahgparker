709fa67ce46efd4e3e5e1c20dd25e24261f6a52c
Sarah Parker <sgparker@live.unc.edu>
2019-01-31 17:58:54 +0000

    quiz 1

---
+++ main.hs
@@ -0,0 +1,5 @@
+finalGrade :: (Integral a) => [a] -> [a] -> a
+
+finalGrade grades weights = gradesum `div` weightsum
+  where gradesum = sum [fst(x) + snd(x) | x <- (zip grades weights)]
+        weightsum = sum weights    
